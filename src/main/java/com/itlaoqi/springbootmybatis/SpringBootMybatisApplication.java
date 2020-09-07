706fdc7e4ca1a5fd67b05acb8cd3e44ca13593b6
jack-chen007 <1459275029@qq.com>
2020-09-07 22:11:59 +0800

    test4

--- src/main/java/com/itlaoqi/springbootmybatis/SpringBootMybatisApplication.java
+++ src/main/java/com/itlaoqi/springbootmybatis/SpringBootMybatisApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 测试111
- *test
+ *test2222
  */
 @SpringBootApplication
 //MapperScan注解会在SpringBoot启动的时候扫描mapper包，并根据xml自动生成对应的实现类
